eae3fa2cbd675973a833688fedd41b3e51e8db67
BlackCat <113409167+ChuHyeonJeong75@users.noreply.github.com>
2022-11-08 17:10:56 +0900

    Initial commit

---
+++ src/main/java/com/example/sbmart/SbmartApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbmart/SbmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
